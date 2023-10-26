16a2cba431737339431993902bb1d0c370149446
Anis Baccari <abaccari@fu-r9-p6.s19.be>
2023-10-26 14:39:24 +0200

    simple test

---
+++ simple.c
@@ -0,0 +1,51 @@
+
+/* 
+	add includes / path to libft
+ */
+#include "simple_test.h"
+
+
+
+
+int main()
+{ 
+const char s1[] = "atoms\0\0\0\0";  // extra null bytes at end
+const char s2[] = "atoms\0abc";     // embedded null byte
+const char s3[] = "atomsaaa";
+
+if(strcmp(s1, s2) == 0)      // strcmp stops at null terminator
+	printf(" test 1 : passed"); 
+else 
+	printf(" test 1 : failed"); 
+
+
+if (strcmp(s1, s3) != 0 )     // Strings are different
+	printf(" test 2 : passed"); 
+else 
+	printf(" test 2 : failed");
+
+if (strncmp(s1, s3, 5) == 0)  // First 5 characters of strings are the same
+printf(" test 3 : passed"); 
+else 
+	printf(" test 3 : failed");
+
+if (memcmp(s1, s3, 5) == 0 )  // First 5 bytes are the same
+printf(" test 4 : passed"); 
+else 
+	printf(" test 4 : failed");
+
+if (strncmp(s1, s2, 8) == 0)  // Strings are the same up through the null terminator
+printf(" test 2 : passed"); 
+else 
+	printf(" test 2 : failed");
+
+if (memcmp(s1, s2, 8) != 0)   // First 8 bytes are different
+printf(" test 2 : passed"); 
+else 
+	printf(" test 2 : failed");
+
+return 0;
+}
+
+
+
---
+++ simple_test.h
@@ -0,0 +1,9 @@
+#ifndef	SIMPLE_TEST_H
+#define SIMLPE_TEST_H
+#endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+
